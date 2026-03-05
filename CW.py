b7e346e36be4681fcb54678905e171fae995dbe3
DylanQ688 <heinerqs@gmail.com>
2026-03-05 18:22:18 -0500

    Create CW.py

---
+++ CW.py
@@ -0,0 +1,12 @@
+import random
+
+caracteres = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+longitud = int(input("Introduce la longitud de la contraseña: "))
+
+contrasena = ""
+
+for i in range(longitud):
+    contrasena += random.choice(caracteres)
+
+print("Tu contraseña generada es:", contrasena)
